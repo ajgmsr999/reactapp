e91633a1c8790bdfa9d7d1abd755d07a99471644
ajgmsr999 <ajgmsr00@gmail.com>
2021-12-04 17:05:09 +0530

    react app create

---
+++ src/MyComponents/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Footer = () => {
+    return (
+        <div>
+             <p className="text-center">
+            Copyright &copy; MyTodosList.com
+            </p>
+        </div>
+    )
+}
